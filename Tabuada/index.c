0cc37d3641fde2267166bf1730b8abd515b892f7
Vinicius R Pilati <vini1718a@gmail.com>
2023-12-05 20:03:50 -0300

    Exercicio da tabuada completo

--- Tabuada/index.c
+++ Tabuada/index.c
@@ -4,15 +4,20 @@
 int main() {
     printf("Aqui esta a tabuada: \n");
 
-    int resultado[9];
+    int resultado[11];
+    int totalDaSoma = 0;
     
     for(int i = 1;i <= 10; i++) {
-        for(int j = 1; j <= 10;j++) {
-            int total = i * j;
-        printf("O resultado de %d x %d = %d \n", i, j, total);
-        }
-        //printf("\n Esse e o total da soma: %d \n", resultado[0]);
+        resultado[i] = 0;
+            for(int j = 1; j <= 10;j++) {
+                int total = i * j;
+                printf("O resultado de %d x %d = %d \n", i, j, total);
+                resultado[i] = resultado[i] + total;  
+                totalDaSoma = totalDaSoma + total; 
+            }
+                printf("\n---------------- O total das somas da tubuada do %d e: %d\n",i, resultado[i]);
     }
+    printf("O total da soma das tabuadas e: %d", totalDaSoma);
     return 0;
 }
 
