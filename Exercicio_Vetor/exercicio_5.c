cf32511186cbe590dde9c9d9c2ab817f424e847a
Vinicius R Pilati <vini1718a@gmail.com>
2023-12-07 22:01:44 -0300

    Resolução exercicio 5

--- Exercicio_Vetor/exercicio_5.c
+++ Exercicio_Vetor/exercicio_5.c
@@ -4,22 +4,29 @@
 
 int main() {
     int numbers[5];
-    int maior = 0;
-    int menor = 7000;
+    int maior;
+    int menor;
+    int PosicMenor;
+    int PosicMaior;
 
     printf("Digite os valores em cada posicao do vetor\n");
     
     for(int i = 0;i<=4;i++) {
         scanf("%d", &numbers[i]);
+    }
 
+    maior = numbers[0];
+    menor = numbers[0];
+
+     for(int i = 0;i<=4;i++) {
          if(numbers[i] > maior) {
-             numbers[i] = maior;
-             printf("Esse e o maior: %d \n", i);
-
+            maior = numbers[i];  
+            PosicMaior = i;
         }
         if(numbers[i] < menor) {
-             menor = i;  
+             menor = numbers[i];  
+             PosicMenor = i;
         }  
-    }
-    printf("O maior e o: numbers[%d] e o menor e: numbers[%d]", maior,menor);
+     }
+    printf("O maior e o: posicao[%d] e o menor e: posicao[%d]", PosicMaior,PosicMenor);
 }    
